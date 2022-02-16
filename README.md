df842ee259f41808766991eb545316528965266d
jordydp <jdepril@outlook.com>
2022-02-16 12:24:44 +0100

    Working with IF ELSE

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # verou-3-the-dream-Jordydp
+ Japanese yen = 0,0076 Euro
+ 1 swiss frank = 0.95 eur
+ 1 canadese dollar = 0.69 eur
--- index.php
+++ index.php
@@ -2,14 +2,58 @@
 //var_dump($_POST);
 //echo $_POST["destination_currency"];
 $yen = 131.78;
+$canadianDollar =1.44;
+$swissFrank = 1.05;
 $euro = intval($_POST["destination_currency"]);
-$result = $euro * $yen;
-$resultString = strval($result);
-echo "<br>"."you will get " . $result . " Japanese yen";
-?>
 
-<form action="" method="post">
-    <label for="destination_currency">How much euro's you want to spend in Japan?</label>
+if($_POST["currencytype"] =="Japan"){
+    $result = $euro * $yen;
+    $resultString = "<br>you will get " . number_format($result, 2, ',', ' ') .  " Japanese yen!";
+
+}else if($_POST["currencytype"] =="Canada")
+{
+    $result = $euro * $canadianDollar;
+    $resultString = "<br> You will get ". number_format($result,2, ',', ' ') . " Canadian Dollars";
+
+}else if($_POST["currencytype"] =="Switzerland")
+{
+    $result = $euro * $swissFrank;
+    $resultString = "<br> You will get ". number_format($result,2, ',', ' ') . " Swiss Franc";
+    
+}else
+{
+    $resultString= "Select your Final destination";
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body align="center">
+<form action="" method="post"> 
+    <label for="destination_currency"><h1>How much euro's you want to spend in<h1></label><br>
+    <input type="radio" id="japan" name="currencytype" value="Japan">Japan<br>
+    <input type="radio" id="Canada" name="currencytype" value="Canada">Canada<br>
+    <input type="radio" id="Switzerland" name="currencytype" value="Switzerland">Switzerland<br>
     <input type="number" name="destination_currency" id="destination_currency"  /><br>
     <input type ="submit">
+    <div class="result">
+        <?=$resultString?>
+    </div>
 </form>
+<div>
+
+</div>
+</body>
+</html>
+
+
+
+
+
+
